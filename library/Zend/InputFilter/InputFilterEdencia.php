8e920430f1be725351d7306a2088cbcf63e69ff7
Vincent Caggiari <vincent@edencia.com>
2014-09-03 12:35:33 +0200

    Update InputFilterEdencia.php
    Add continue if empty to all inputFilters

--- library/Zend/InputFilter/InputFilterEdencia.php
+++ library/Zend/InputFilter/InputFilterEdencia.php
@@ -2,7 +2,16 @@
 
 namespace Zend\InputFilter;
 
-class InputFilterEdencia extends InputFilter
+class InputFilterEdencia extends InputFilter implements EmptyContextInterface
 {
+  protected $continueIfEmpty;
 
+  public function setContinueIfEmpty($continueIfEmpty) {
+    $this->continueIfEmpty = $continueIfEmpty;
+
+    return $this;
+  }
+  public function continueIfEmpty() {
+    return $this->continueIfEmpty;
+  }
 }
